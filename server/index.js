e57c4a2361b2abe6cf8beaea3824fcf732331796
Rishika kushwah <145997382+kushwahrishu@users.noreply.github.com>
2025-05-18 22:57:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,13 +11,14 @@ dotenv.config();
 
 dbConnection();
 
-const PORT = process.env.PORT || 5000;
+ const PORT = process.env.PORT || 5000;
+
 
 const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: 'https://taskify-client-ozqo.onrender.com',
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
